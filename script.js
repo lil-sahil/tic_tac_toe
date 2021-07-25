8eb2edeb7093c7df81ccfc8ff97adf32c216a526
lil-sahil <sahilnagpal93@yahoo.ca>
2021-07-25 16:31:49 -0400

    Added basic styling. Added playerFactory factory, gameBoard module which is responsible for adding markers to the game grid and displaying the markers, gameController module which is resposible for gamelogic and player choice.

--- script.js
+++ script.js
@@ -1,28 +1,78 @@
 // Player Factory
 const createPlayer = (marker) => {
     const getMarker = () => marker;
-
     return {getMarker}
 }
 
 
 // Gameboard Module
-
 const gameBoard = (() => {
+    const boxes = document.querySelectorAll('.box');
+
     let gameArray = ['','','','','','','','',''];
 
-    const placemarker = (index, marker) => {
-        gameArray[index] = marker};
+    const setGameArray = () => {
+        gameArray = ['','','','','','','','',''];
+    }
     
-    return { placemarker };
+
+    // Place marker
+    const placemarker = (index, marker) => {
+        gameArray[index] = marker;
+        displayMarkers();
+    };
+    
+    // display current markers
+    const displayMarkers = () => { 
+        for (let index = 0; index < gameArray.length; index++){
+            
+            boxes.forEach(box => {
+                if (parseInt(box.id.split('-')[1]) === index){
+                    box.innerHTML = gameArray[index];
+                }        
+            });
+        } 
+    };
+
+
+    return { placemarker , setGameArray, displayMarkers};
 
 })();
+
 
 // game controller
 
 const gameController = (() => {
+
+    // Initialize Players
     const playerX = createPlayer('X');
     const playerO = createPlayer('O');
 
+    // Round Logic
     let pickNumber = 1;
+
+    // Get gameboard elements
+    const boxes = document.querySelectorAll('.box');
+
+    boxes.forEach(box => {
+        box.addEventListener('click', (e) => {
+            let index = parseInt((e.target.id).split('-')[1]);
+            gameBoard.placemarker(index, playerX.getMarker());
+        });
+    });
+    
+
+
+
+})();
+
+// Reset Game
+const resetButton = (() => {
+    const resetButton = document.querySelector('.reset-button');
+
+    resetButton.addEventListener('click', () => {
+        gameBoard.setGameArray()
+        gameBoard.displayMarkers();
+    })
+
 })();
